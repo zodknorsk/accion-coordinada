0e5b8aa81a25018da791b7570b2040788ad5dc05
Hegoi <zodknorsk@gmail.com>
2026-09-23 17:15:52 +0200

    Acción coordinada de iluminante v1.0

---
+++ sw.js
@@ -0,0 +1,15 @@
+var CACHE = 'coordinada-1.0-b583fdc79142';
+var ARCHIVOS = ['./', 'index.html', 'manifest.webmanifest', 'icono-180.png', 'icono-192.png', 'icono-512.png'];
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(ARCHIVOS); }).then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (nombres) {
+    return Promise.all(nombres.filter(function (n) { return n !== CACHE; }).map(function (n) { return caches.delete(n); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+// Primero lo guardado: sin conexión abre al momento
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request, { ignoreSearch: true }).then(function (r) { return r || fetch(e.request); }));
+});
